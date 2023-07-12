f5c0dc62cc11e7e40b06c8929fd229b5a025da79
Melissa Ridente <melissaridente3e@gmail.com>
2023-07-12 13:45:02 +0200

    changes-query600

--- src/CV/cv.js
+++ src/CV/cv.js
@@ -60,7 +60,7 @@ export const CV = {
       link: ''
     },
     {
-      name: "Confecciones Ana Gomes",
+      name: "Confec. Ana Gomes",
       description: 'Made with React, using an API to create a fake online store, in which you can log in while being stored in localStorage, having a letter for each product, implementing libraries such as antDesign as well as ReactRedux, hooks, useState.',
       img: 'https://pbs.twimg.com/media/Fw-OgrpWcAIe-cU?format=png&name=small',
       img2: 'https://pbs.twimg.com/media/FxUA0RFWIAEjD1Z?format=jpg&name=medium',
@@ -75,6 +75,13 @@ export const CV = {
     },
     {
       name: "Corteingles",
+      description: 'Made with React, as a final project of the Bootcamp that creates an informative page, about flights and cruises that could be done in Spain, some tourist information about these places as well. I implemented libraries, React Redux, useProps, useRef, useState, a JSON document that had all my information.',
+      img: 'https://pbs.twimg.com/media/Fw-NRigWIAAwW7c?format=png&name=small',
+      img2: 'https://pbs.twimg.com/media/FxUHG8FWIAMh7UG?format=jpg&name=large',
+      link: ''
+    },
+    {
+      name: "PokeApi",
       description: 'Made with React, as a final project of the Bootcamp that creates an informative page, about flights and cruises that could be done in Spain, some tourist information about these places as well. I implemented libraries, React Redux, useProps, useRef, useState, a JSON document that had all my information.',
       img: 'https://pbs.twimg.com/media/Fw-NRigWIAAwW7c?format=png&name=small',
       img2: 'https://pbs.twimg.com/media/FxUHG8FWIAMh7UG?format=jpg&name=large',
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -24,12 +24,28 @@ const Experience = () => {
                 return (
                   <div key={JSON.stringify(item)} className={styles.Cards}> 
                     <img src={item?.img2} className={styles.IMG}/>
-                    <a href='https://www.linkedin.com/in/melissa-ridente/' target='_blank' className={styles.Visit}><p><ShareAltOutlined /> VISIT WEB</p></a>
+                    <a href='https://www.linkedin.com/in/melissa-ridente/' target='_blank' className={styles.Visit}><p><ShareAltOutlined /> VISIT WEB</p></a> 
                   </div>
                 );
               })}
             </Carousel>
           </div>
+          <div className={styles.Min600}>
+            <div className={styles.First}>
+              <h1 className={styles.Title1}>MY WORK</h1>
+              <div className={styles.Line}></div>
+            </div>
+            {experience?.map((item) => {
+              return (
+                <div key={JSON.stringify(item)} className={styles.Card}>
+                  <div className={styles.Dentro}> 
+                    <p className={styles.Names}>✓{item.name}</p>
+                    <a href='https://www.linkedin.com/in/melissa-ridente/' className={styles.visit}><p target='_blank'><ShareAltOutlined /> VISIT WEB.</p></a>
+                  </div>
+                </div>
+              );
+            })}
+          </div> 
         </div>
       </div>
     </>
