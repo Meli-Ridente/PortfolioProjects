b1f48b2aa26bff948306d7611152b5ee45a02141
Melissa Ridente <melissaridente3e@gmail.com>
2023-07-21 14:20:56 +0200

    .

--- src/CV/cv.js
+++ src/CV/cv.js
@@ -47,7 +47,7 @@ export const CV = {
   ],
   experience: [
     {
-      id: 5,
+      id: 1,
       name: "PokeApi",
       description: 'Made with native Javascript I developed a project with the Pokemon API data. I designed the collectible pokemon cards that each pokemon has, being able to add favorites, filter a search and see the 151 cards.',
       img: 'https://pbs.twimg.com/media/Fw-NRigWIAAwW7c?format=png&name=small',
@@ -56,7 +56,7 @@ export const CV = {
       link2: 'https://github.com/Meli-Ridente/PokeApi'
     },
     {
-      id: 5,
+      id: 2,
       name: "Corteingles",
       description: 'Made with React, as a final project of the Bootcamp that creates an informative page, about flights and cruises that could be done in Spain, some tourist information about these places as well. I implemented libraries, React Redux, useProps, useRef, useState, a JSON document that had all my information.',
       img: 'https://pbs.twimg.com/media/Fw-NRigWIAAwW7c?format=png&name=small',
@@ -65,7 +65,7 @@ export const CV = {
       link2: 'https://github.com/Meli-Ridente/corte-ingles1'
     },
     {
-      id: 2,
+      id: 3,
       name: "Netflix",
       description: 'Made with AngularJs, I designed a project based on NETFLIX, in which you can watch the top movies of the moment, comedies, or some other categories.',
       img: 'https://pbs.twimg.com/media/Fw53xuuWYAIttzO?format=png&name=small',
@@ -74,7 +74,7 @@ export const CV = {
       link2: 'https://github.com/Meli-Ridente/NetflixApp'
     },
     {
-      id: 3,
+      id: 4,
       name: "Confec. Ana Gomes",
       description: 'Made with React, using an API to create a fake online store, in which you can log in while being stored in localStorage, having a letter for each product, implementing libraries such as antDesign as well as ReactRedux, hooks, useState.',
       img: 'https://pbs.twimg.com/media/Fw-OgrpWcAIe-cU?format=png&name=small',
@@ -83,7 +83,7 @@ export const CV = {
       link2: 'https://github.com/Meli-Ridente/My-Store'
     },
     {
-      id: 4,
+      id: 5,
       name: "TravelSpain",
       description: 'Made with React, as a final project of the Bootcamp that creates an informative page, about flights and cruises that could be done in Spain, some tourist information about these places as well. I implemented libraries, React Redux, useProps, useRef, useState, a JSON document that had all my information.',
       img: 'https://pbs.twimg.com/media/Fw-NRigWIAAwW7c?format=png&name=small',
@@ -92,7 +92,7 @@ export const CV = {
       link2: 'https://github.com/Meli-Ridente/TravelSpainProyectFinal'
     },
     {
-      id: 1,
+      id: 6,
       name: "Pokedex",
       description: 'Made with native React I developed a project with the Pokemon API data. I designed the collectible pokemon cards that each pokemon has, being able to add favorites, filter a search and see the 151 cards.',
       img: 'https://pbs.twimg.com/media/F1ZjeHRWYAYWAyB?format=jpg&name=large',
